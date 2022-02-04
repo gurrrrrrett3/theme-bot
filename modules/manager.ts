6012e1536508ba2d113a6e014e231ac35de01ce7
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2022-02-03 23:15:59 -0500

    bump version

--- modules/manager.ts
+++ modules/manager.ts
@@ -43,7 +43,7 @@ export default class Manager {
         this.audioPlayer.play(resource)
         resource.volume?.setVolume((resource.metadata.volume / 100) * (maxVolume / 100))
         
-        if (resource.playbackDuration > parseInt(resource.metadata.playTime.toString()) {
+        if (resource.playbackDuration > parseInt(resource.metadata.playTime.toString())) {
             DataManager.setPlayTime(resource.metadata.userID, resource.playbackDuration)
         }
 
