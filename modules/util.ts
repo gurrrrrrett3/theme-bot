08b60c0109919f70b2b2e2c171bba934d008e1f7
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2022-08-01 22:57:20 -0400

    goodbye theme bot

--- modules/util.ts
+++
@@ -1,9 +0,0 @@
-import { ThemeType } from "./dataManager"
-
-export default class Util {
-    public static checkType(type: string): ThemeType {
-        if (!type) return "ENTER"
-        if (type.toLowerCase() === "exit") return "EXIT"
-        else return "ENTER"
-    }
-}
